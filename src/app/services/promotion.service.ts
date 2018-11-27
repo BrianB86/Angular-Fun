b58e9f0eaf4d3b67c2098262a6c153ea8565b2dc
Brian <bbattell@gmail.com>
2018-11-26 20:58:18 -0500

    Added more animations and started to use the promotions REST service.

--- src/app/services/promotion.service.ts
+++ src/app/services/promotion.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Promotion } from '../shared/promotion';
-import { PROMOTIONS } from '../shared/promotions';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { baseURL } from '../shared/baseurl';
+import { map, catchError } from 'rxjs/operators';
+import { ProcessHTTPMsgService } from './process-httpmsg.service';
 import { Observable, of } from 'rxjs';
 import { delay } from 'rxjs/operators';
 
@@ -9,17 +12,21 @@ import { delay } from 'rxjs/operators';
 })
 export class PromotionService {
 
-  constructor() { }
+  constructor(private http: HttpClient, private processHTTPMsgService: ProcessHTTPMsgService) { }
 
   getPromotions(): Observable<Promotion[]> {
-    return of(PROMOTIONS).pipe(delay(2000));
+    return this.http.get<Promotion[]>(baseURL + 'promotions')
+      .pipe(catchError(this.processHTTPMsgService.handleError));
   }
 
-  getPromotion(id: number): Observable<Promotion> {
-    return of(PROMOTIONS.filter((promo) => (promo.id === id))[0]).pipe(delay(2000));
+  getPromotion(id: string): Observable<Promotion> {
+    return this.http.get<Promotion>(baseURL + 'promotions/' + id)
+      .pipe(catchError(this.processHTTPMsgService.handleError));
   }
 
   getFeaturedPromotion(): Observable<Promotion> {
-    return of(PROMOTIONS.filter((promotion) => promotion.featured)[0]).pipe(delay(2000));
+    return this.http.get<Promotion[]>(baseURL + 'promotions?featured=true')
+      .pipe(map(promotions => promotions[0]))
+      .pipe(catchError(this.processHTTPMsgService.handleError));
   }
 }
